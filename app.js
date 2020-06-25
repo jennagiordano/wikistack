730d857e1738e75fc043a2f3c22600a35382a315
Jenna Giordano <jgiordano17@fordham.edu>
2020-06-25 18:03:08 -0400

    WIP  - Wikistack Solutions

--- app.js
+++ app.js
@@ -1,45 +1,15 @@
-const express = require("express");
-const morgan = require("morgan");
-const layout = require("./views/layout");
-const models = require("./models");
-const user = require("./routes/user");
-const wiki = require("./routes/wiki");
+"use strict";
 
-const app = express();
+var express = require("express");
+var app = express();
+var routes = require("./routes/index");
 
-//PLUG IN ROUTES
-app.use("/wiki", wiki);
-app.use("/user", user);
+module.exports = app; // this line is only used to make testing easier.
 
-//LOGGING MIDDLEWARE
-app.use(morgan("dev"));
-
-//STATIC MIDDLEWARE
-app.use(express.static(__dirname + "/public"));
-
-//BODY PARSING MIDDLEWARE
+app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use("/users", routes);
 
-app.get("/", (req, res, next) => {
-  try {
-    res.redirect("/wiki");
-  } catch (error) {
-    next(error);
-  }
-});
+// remember to plug in your router and any other middleware you may need here (i.e. body parser, mounting any router-level middleware, etc.)
 
-models.db.authenticate().then(() => {
-  console.log("connected to the database");
-});
-
-const PORT = 3000;
-
-const init = async () => {
-  await models.db.sync({ force: true });
-
-  app.listen(PORT, () => {
-    console.log(`App listening in port ${PORT}`);
-  });
-};
-
-init();
+if (!module.parent) app.listen(3000); // conditional prevents a very esoteric EADDRINUSE issue with mocha watch + supertest + npm test.
